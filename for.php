6f3005c6ead9e162fba59537587264924b9dd5a1
RicoAgnis <ricoagnis12@gmail.com>
2016-03-27 15:27:01 +0700

    Added files via upload

---
+++ for.php
@@ -0,0 +1,50 @@
+<html> 
+<body>
+<title> For </title>
+   <h1> For </h1>
+<?php
+for ($i= 1; $i <= 1; $i++)
+{
+   echo "1";
+   echo "<br />";
+   echo "<br />";
+}
+for ($i= 2; $i <= 3; $i++)
+{
+   echo "2 ";
+
+  
+}
+ echo "<br/>";
+ echo "<br/>";
+ 
+ for ($i= 3; $i <= 5; $i++)
+{
+   echo "3 ";
+
+  
+}
+ echo "<br/>";
+ echo "<br/>";
+ 
+ for ($i= 4; $i <= 7; $i++)
+{
+   echo "4 ";
+
+  
+}
+ echo "<br/>";
+ echo "<br/>";
+ 
+ for ($i= 5; $i <= 9; $i++)
+{
+   echo "5 ";
+
+  
+}
+ echo "<br/>";
+ echo "<br/>";
+
+?>
+<body> 
+</html>
